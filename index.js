d11798ec98116b0049d5719b8ddb0172cfe2f545
Sasha Sorokin <sorokin.sh@gmail.com>
2019-12-23 21:48:53 +0300

    cow: moooo

---
+++ index.js
@@ -0,0 +1,53 @@
+const http = require('http');
+const https = require('https');
+const { parse } = require('url');
+
+const get_api_url = (id) => `https://www.googleapis.com/youtube/v3/videos?id=${id}&key=AIzaSyBZYeP92jwctxWVI5WwHHP_t0M-xUH4Pf8&part=statistics`;
+
+function handle_request(id, resolve, reject) {
+  https
+    .get(get_api_url(id), (res) => {
+      let rawData = '';
+
+      res.setEncoding('utf8');
+
+      res.on('data', (chunk) => {
+        rawData += chunk;
+      });
+      res.on('end', () => {
+        try {
+          const parsedData = JSON.parse(rawData);
+
+          resolve(parsedData.items[0].statistics);
+        } catch (e) {
+          reject(e);
+        }
+      });
+    })
+    .on('error', reject);
+}
+
+exports.cow = async (req, res) => {
+  const { query: { id } } = parse(req.url, true);
+
+  res.set('Access-Control-Allow-Origin', '*');
+
+  if (req.method === 'OPTIONS') {
+    // Send response to OPTIONS requests
+    res.set('Access-Control-Allow-Methods', 'GET');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Max-Age', '3600');
+    res.status(204).send('');
+    return;
+  }
+
+  try {
+    const result = await new Promise((resolve, reject) => handle_request(id, resolve, reject));
+
+    res.end(JSON.stringify(result));
+  } catch (error) {
+    console.log(error)
+
+    res.end();
+  }
+};
